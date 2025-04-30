1f54f6e0437b3403a2717bf2f3f0a5d8e4efaf4d
Ryan Cheng <ryancheng@berkeley.edu>
2025-04-30 04:18:21 -0700

    added example RL training scripts

---
+++ example_kto_training.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Example KTO training pipeline, perhaps easier to run each command in order rather than editing this file
+# edit relevant variables
+
+conda activate openrlhf
+
+# modify task flag variable with one of [Chatting, Education, Therapy]
+# other tasks currently still need to be supported in jsonl_gen.py
+# takes in files in training_data/in and outputs training jsons in training_data/out
+python jsonl_gen.py --task=Chatting
+
+# specify gpu numbers to host on after localhost:
+nohup deepspeed --include localhost:0,1 --master_port 61000 --module openrlhf.cli.train_kto \
+   --save_path /raid/users/ryan_cheng/checkpoints/Chatting/llama3-8b-kto-49k-prompt \  # around 15 gb
+   --save_steps -1 \
+   --logging_steps 1 \
+   --eval_steps -1 \
+   --train_batch_size 256 \
+   --micro_train_batch_size 1 \
+   --pretrain meta-llama/Meta-Llama-3.1-8B-Instruct \
+   --bf16 \
+   --max_epochs 1 \
+   --max_len 8192 \
+   --zero_stage 3 \
+   --learning_rate 5e-7 \
+   --dataset json@./training_data/out \  # directory with train.jsonl and test.jsonl
+   --input_key in_text \
+   --output_key out_text \
+   --label_key score \
+   --flash_attn \
+   --beta 0.1 \
+   --gradient_checkpointing \
+   --use_wandb 1e3fbbf6aeaa60fb339e7c43b375cb2be8aa7f5f > kto.out & # wandb key to monitor run stats
---
+++ example_ppo_training.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Example PPO training pipeline, perhaps easier to run each command in order rather than editing this file
+# edit relevant variables
+
+conda activate openrlhf
+
+# modify task flag variable with one of [Chatting, Education, Therapy]
+# other tasks currently still need to be supported in jsonl_gen.py
+# takes in files in training_data/in and outputs training jsons in training_data/out
+python jsonl_gen.py --task=Chatting
+
+# start a vllm server for calculating the reward
+# edit port as needed, SHOULD BE THE SAME AS IN THE REWARD FUNCTION SCRIPT!
+# edit CUDA_VISIBLE_DEVICES to choose GPUs to host on (PPO requires at least one other separate one)
+ CUDA_VISIBLE_DEVICES=0,1 nohup vllm serve meta-llama/Meta-Llama-3.1-70B-Instruct --port=8001 --download_dir=/nfs/kun2/users/ryan_cheng/models > llama_reward_server.out &
+
+# start a ray session to host PPO model training
+# all models can be put on a single H200 gpu, might need more for H100 or A100
+# dashboard port must match --address in below command, other ports can vary if necessary
+ CUDA_VISIBLE_DEVICES=2 ray start --head --node-ip-address 0.0.0.0 --dashboard-port=8270 --port=6383  --dashboard-agent-listen-port=52367 --num-gpus 1 --temp-dir=/raid/users/ryan_cheng/tmp
+
+nohup ray job submit --address="http://127.0.0.1:8270" \
+    --runtime-env-json='{"working_dir": "./openrlhf"}' \  # working_dir might need to exist/have enough space but i'm not sure
+    -- python3 -m openrlhf.cli.train_ppo_ray \
+    --ref_num_nodes 1 \
+    --ref_num_gpus_per_node 1 \
+    --critic_num_nodes 1 \
+    --critic_num_gpus_per_node 1 \
+    --actor_num_nodes 1 \
+    --actor_num_gpus_per_node 1 \
+    --vllm_num_engines 1 \
+    --vllm_tensor_parallel_size 1 \
+    --colocate_all_models \  # keeps all of the above models on the same gpu as opposed to gpus_per_node * num_node gpus per model
+    --ref_reward_offload \  # use a custom reward
+    --pretrain meta-llama/Meta-Llama-3.1-8B-Instruct \  # pretrained model, can be specified as an actual file directory to saved checkpoints
+    --remote_rm_url ./reward_func_prompt.py \  # path to a py file with reward_func defined to use in calculating rewards
+    --save_path /raid/users/ryan_cheng/checkpoints/Chatting/llama-8b-ppo-prompt \  # checkpoints are ~15 gb
+    --micro_train_batch_size 8 \
+    --train_batch_size 128 \
+    --micro_rollout_batch_size 16 \
+    --rollout_batch_size 1024 \
+    --max_samples 100000 \
+    --max_epochs 1 \
+    --prompt_max_len 1024 \
+    --generate_max_len 1024 \
+    --packing_samples \
+    --zero_stage 3 \
+    --bf16 \
+    --actor_learning_rate 5e-7 \
+    --critic_learning_rate 9e-6 \
+    --init_kl_coef 0.01 \
+    --prompt_data json@./training_data/out \  # directory with train.jsonl and test.jsonl
+    --input_key in_text \
+    --normalize_reward \
+    --adam_offload \
+    --flash_attn \
+    --gradient_checkpointing \
+    --save_steps 10 \  # save checkpoints every 10 steps. NOTE: i've run into model crashes in saving checkpoints so maybe remove if that is an issue
+    --max_ckpt_num 3 \  # max number of checkpoints to keep at a time (each quite large, 20-30 gb i think) (remove if not saving)
+    --ckpt_path /raid/users/ryan_cheng/checkpoints/Chatting/checkpoints/llama-8b-ppo-prompt \
+    --save_hf_ckpt \  # remove if not saving
+    --use_wandb 1e3fbbf6aeaa60fb339e7c43b375cb2be8aa7f5f > ppo.out & # wandb key to monitor run stats
